13d12e0c7e15f8077cca5ec2c6b9efb0f6fb42c7
ankit-1k <ankitpanda922@gmail.com>
2025-02-15 23:24:11 +0530

    index.html added bootstrap icons and some changes on footer

--- src/component/Footer/Footer.jsx
+++ src/component/Footer/Footer.jsx
@@ -1,24 +1,34 @@
 import React from "react";
 import "./Footer.css";
 const Footer = () => {
-  return <div>
-     <div>
-      <div className="bg-yellow">
-        <div className="container">
-          <div className="col-md-3">
-            <img className='logo-footer-img' src="https://playschool.ingridtemplates.com/wp-content/uploads/sites/10/elementor/thumbs/lego-blocks-oxktkzhmsrky4ynj8aul92ie1zkfsl6h3tm39scelk.png" alt="" />
-            
-          </div>
-          <div className="col-md-4">
-
-          </div>
-          <div className="col-md-5">
-
+  return (
+    <div>
+      <div>
+        <div className="bg-yellow">
+          <div className="container">
+            <div className="col-md-3">
+              <div className="d-flex align-items-center">
+                <img
+                  className="logo-footer-img"
+                  src="https://playschool.ingridtemplates.com/wp-content/uploads/sites/10/elementor/thumbs/lego-blocks-oxktkzhmsrky4ynj8aul92ie1zkfsl6h3tm39scelk.png"
+                  alt=""
+                />
+                <div className="title ms-3">Playschool</div>
+              </div>
+              <div className="row">
+                <div className="col-3">
+                  <i class="bi bi-telephone-fill"></i>
+                </div>
+                <div className="col-9">641-754-0072</div>
+              </div>
+            </div>
+            <div className="col-md-4"></div>
+            <div className="col-md-5"></div>
           </div>
         </div>
       </div>
     </div>
-  </div>;
+  );
 };
 
 export default Footer;
--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import "./Header.css";
+import Footer from "../Footer/Footer";
 
 const Header = () => {
   return <div>
-    
+    <Footer />
   </div>;
 };
 
