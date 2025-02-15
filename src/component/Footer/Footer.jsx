bb5a5e8f9bd89954e0fde6ee216bce1ec62df9ac
ankit-1k <ankitpanda922@gmail.com>
2025-02-15 22:42:55 +0530

    footer

--- src/component/Footer/Footer.jsx
+++ src/component/Footer/Footer.jsx
@@ -1,7 +1,24 @@
 import React from "react";
 import "./Footer.css";
 const Footer = () => {
-  return <div>Footer</div>;
+  return <div>
+     <div>
+      <div className="bg-yellow">
+        <div className="container">
+          <div className="col-md-3">
+            <img className='logo-footer-img' src="https://playschool.ingridtemplates.com/wp-content/uploads/sites/10/elementor/thumbs/lego-blocks-oxktkzhmsrky4ynj8aul92ie1zkfsl6h3tm39scelk.png" alt="" />
+            
+          </div>
+          <div className="col-md-4">
+
+          </div>
+          <div className="col-md-5">
+
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>;
 };
 
 export default Footer;
