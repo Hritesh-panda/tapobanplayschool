95bcd99792f45bb26a823f07bb9c4742c0adc362
ankit-1k <ankitpanda922@gmail.com>
2025-02-15 22:54:36 +0530

    footer test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import './App.css'
+import Header from './component/Header/Header'
 const App = () => {
   return (
     <div>
-      
+      <Header />
     </div>
   )
 }
