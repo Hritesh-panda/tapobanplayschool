8eee7d414abbe73905bd4147de2579d1573fc87e
ankit-1k <ankitpanda922@gmail.com>
2025-02-23 11:27:32 +0530

    Contact start

---
+++ src/component/Contact/Contact.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div>
+      Contact JSX
+    </div>
+  )
+}
+
+export default Contact
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Footer from "../../component/Footer/Footer";
 import Navbar from "../../component/navbar/Navbar";
+import Contact from "../../component/Contact/Contact";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
+      <Contact />
       <Footer />
     </div>
   );
