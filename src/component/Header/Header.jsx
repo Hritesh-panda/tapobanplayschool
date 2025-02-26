86063acff5507137295635584b476acc718ac53e
= <=>
2025-02-27 01:05:59 +0530

    header impliment

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -1,10 +1,44 @@
 import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import HeroOne from "../../asset/hero2.jpg";
+import HeroTwo from "../../asset/hero3.jpg";
+import Herothree from "../../asset/hero4.jpg";
 import "./Header.css";
 
+import "swiper/css";
+import "swiper/css/effect-fade";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import { EffectFade, Navigation, Pagination, Autoplay } from "swiper/modules";
 const Header = () => {
   return (
     <div className="header">
-      <div className="container"></div>
+      <Swiper
+        spaceBetween={30}
+        effect={"fade"}
+        navigation={true}
+        centeredSlides={true}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[EffectFade, Navigation, Pagination, Autoplay]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <img src={HeroOne} alt="NO image found" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={HeroTwo} alt="No image found" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={Herothree} alt="No image found" />
+        </SwiperSlide>
+      </Swiper>
     </div>
   );
 };
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import Footer from "../../component/Footer/Footer";
 import Navbar from "../../component/navbar/Navbar";
 import Contact from "../../component/Contact/Contact";
+import Header from "../../component/Header/Header";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
+      <Header />
       <Contact />
       <Footer />
     </div>
